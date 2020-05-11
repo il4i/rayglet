9a15434f8cde385bc355c5f90062e4669150fd5d
Ian Lai <ian.lai@oberlin.edu>
2020-05-11 19:40:36 -0400

    TODO: wall_collision()

--- src/rcast.pyx
+++ src/rcast.pyx
@@ -12,11 +12,17 @@ cdef int BLOCK_LWH = 32
 # maximum length of vision in blocks
 cdef int MAX_SIGHT = 8
 
+# enums
+cdef int EMPTY = 0
+cdef int WALL = 1
+
+
 cdef class Raycaster:
 
     cdef unsigned int pov_height  # dimensions of vision
     cdef unsigned int pov_length
 
+    # x and y are represented in fractions of a block
     cdef float player_x, player_y, pov_angle
 
     # When pov_angle = 0, the player is facing to the right.
@@ -38,16 +44,34 @@ cdef class Raycaster:
         
     def load_grid(self, grid):
         self.grid = array.array(*grid)
-    
+
+
+    def wall_collision(self):
+        """ Returns whether or not the player has hit a wall. """
+
+        pass
 
     def move_forward(self, dist):
-        """ Each dist unit is 1/BLOCK_LWH of a block. """
+        """ Each dist unit is 1/BLOCK_LWH of a block. Returns whether or not
+        the movement successfully avoided a wall. """
         self.player_x += (dist / BLOCK_LWH) * math.cos(self.pov_angle)
         self.player_y += (dist / BLOCK_LWH) * math.sin(self.pov_angle)
+
+        if self.wall_collision():
+            self.move_backward(dist)
+            return False
+
+        return True
 
     def move_backward(self, dist):
         self.player_x -= (dist / BLOCK_LWH) * math.cos(self.pov_angle)
         self.player_y -= (dist / BLOCK_LWH) * math.sin(self.pov_angle)
+
+        if self.wall_collision():
+            self.move_forward(dist)
+            return False
+
+        return True
 
     def turn_left(self, angle):
         """ The turn_* methods take angle in degrees. """
@@ -57,9 +81,23 @@ cdef class Raycaster:
         self.pov_angle -= angle * (math.pi / 180)
     
     
-    def ray_cast(angle):
+    def ray_cast(self, angle):
         """ Returns the distance to the nearest wall from angle. """
         cdef int ray_len
+        cdef float orig_x, orig_y, orig_angle
+
+        orig_x = self.player_x
+        orig_y = self.player_y
+        orig_angle = self.pov_angle
+        
+        self.pov_angle = angle
         
         for ray_len in range(0, BLOCK_LWH * MAX_SIGHT):
-            pass
+            if self.move_forward(1) == False:
+                break
+
+        self.player_x = orig_x
+        self.player_y = orig_y
+        self.pov_angle = orig_angle
+
+        return ray_len
