8955ded9160c46d2d7b65b4e1f2d7d7862966c8a
Ian Lai <ian.lai@oberlin.edu>
2020-05-13 09:45:10 -0400

    Press q to quit. You can hold down buttons to move and turn.

--- src/render.py
+++ src/render.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import pyglet 
 import rcast
 from pyglet.window import key 
@@ -27,32 +29,48 @@ def setView():
         drawColumn(dx, output[dx].get_sky(), output[dx].get_wall(),
                    output[dx].get_floor())
 
+
+keys_down = {'A' : False, 'D' : False, 'S' : False, 'W' : False}
+
 #Draws first screen
 @window.event
 def on_draw():
     window.clear()
+    if keys_down['W']:
+        caster.move_forward(4)
+    if keys_down['A']:
+        caster.turn_left(3)
+    if keys_down['S']:
+        caster.move_backward(4)
+    if keys_down['D']:
+        caster.turn_right(3)
     setView()
 
 #Redraws window on key press
 @window.event
 def on_key_press(symbol, modifiers):
-    #window.clear()
     if symbol == key.A:
-        caster.turn_left(17)
-        setView()
-        #turn left
+        keys_down['A'] = True
     elif symbol == key.D:
-        caster.turn_right(17)
-        setView()
-        #turn right
+        keys_down['D'] = True
     elif symbol == key.S:
-        caster.move_backward(8)
-        setView()
-        #move back
+        keys_down['S'] = True
     elif symbol == key.W:
-        caster.move_forward(8)
-        setView()
-        #move forward
+        keys_down['W'] = True
+    elif symbol == key.Q:
+        sys.exit(0)
+
+@window.event
+def on_key_release(symbol, modifiers):
+    if symbol == key.A:
+        keys_down['A'] = False
+    elif symbol == key.D:
+        keys_down['D'] = False
+    elif symbol == key.S:
+        keys_down['S'] = False
+    elif symbol == key.W:
+        keys_down['W'] = False
+
 
 #Column Draw
 def drawColumn(start, sky, wall, ground):
@@ -76,4 +94,13 @@ def drawColumn(start, sky, wall, ground):
                              ('c4B', [102, 178, 255, 0]*4))
 
 #ACTIVATE THE PYGLET
-pyglet.app.run()
+#pyglet.app.run()
+while True:
+    #pyglet.clock.tick()
+    time.sleep(0.0001)
+    
+    for window in pyglet.app.windows:
+        window.switch_to()
+        window.dispatch_events()
+        window.dispatch_event('on_draw')
+        window.flip()
